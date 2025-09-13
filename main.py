8b0f250baa85f696b9c54ca87e984f2f6988d394
theSp0ty <spoty555@yandex.ru>
2025-09-13 04:48:02 +0300

    отладка

--- main.py
+++ main.py
@@ -358,7 +358,7 @@ async def view_weather_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Введите название города для прогноза:")
     save_user_states()
 
-async def main():
+def main():
     load_user_states()
     for user_id, state in user_states.items():
         send_time = state.get("send_time")
@@ -381,5 +381,4 @@ async def main():
     app.run_polling()
 
 if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
+    main()
